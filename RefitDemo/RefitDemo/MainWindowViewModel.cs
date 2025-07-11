6e0a6d207ebc2bbc950fa5f598598cbb834768e5
anyu <glepooek@outlook.com>
2025-07-11 18:12:41 +0800

    refit demo

---
+++ RefitDemo/RefitDemo/MainWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefitDemo
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+
+
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void RaisePropertyChanged([CallerMemberName] string propertyName = "")
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        #endregion
+    }
+}
